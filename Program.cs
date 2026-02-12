f218cb2b9b4e4e9a6e1cf4809f6bb007df73eee3
garysinghtrading-dot <gary.singh.trading@gmail.com>
2026-02-11 17:30:38 -0800

    Update Program.cs
    Added logic to serve razor pages

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.MapGet("/hello", () => "<title>Hello World </title> <h1>Hello World from Azure!</h1>");
+
+app.MapRazorPages();
+
+app.Run();
